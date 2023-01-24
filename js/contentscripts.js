0be8987ed833dc4c2273a18ee3b521b90731cfc6
src <src@srcw.net>
2023-01-24 15:24:43 +0900

    update markup

--- js/contentscripts.js
+++ js/contentscripts.js
@@ -4,22 +4,23 @@
         if (event.keyCode == 87 && !event.shiftKey) {
             chrome.runtime.sendMessage({method: "getOptions"}, function(response) {
                 var tab_count = response.fso_tabcount
-                var entriesPath = '//div[@class="EntryList__chunk"]//article[contains(@class, "entry") and contains(@class, "quicklisted")]';                
+                
+                var entriesPath = '//div[@class="EntryList__chunk"]//div[@class="TitleOnlyEntry"]';
                 var starPath = './/button[contains(@class, "EntryReadLaterButton--saved")]';
-                //                var linkPath = './/a[contains(@class, "entry__title")]';
                 // 2022/12/7
-                var linkPath = './/div[contains(@class, "content")]/a'
+                var linkPath = './/div[@class="TitleOnlyEntry__content"]/a'
                 var entries = document.evaluate(entriesPath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+//                console.log("entries=" + entries.snapshotLength);
                 for (var i = 0, m = tab_count; i < entries.snapshotLength && m > 0; i++) {
                     var entry = entries.snapshotItem(i);
                     m--
                     var stars = document.evaluate(starPath, entry, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-                    console.log("stars=" + stars.snapshotLength);
+//                    console.log("stars=" + stars.snapshotLength);
                     if (stars.snapshotLength  == 0) {
                         continue;
                     }
                     var links = document.evaluate(linkPath, entry, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-                    console.log("links=" + links.snapshotLength);                    
+//                    console.log("links=" + links.snapshotLength);                  
                     if (links.snapshotLength > 0) {
                         var link = links.snapshotItem(0);
                         window.open(link.href);  
