7724497953a38b9d6cd5f6095e8404d7e8615c6c
src <src@srcw.net>
2023-02-05 23:25:00 +0900

    changes

--- js/contentscripts.js
+++ js/contentscripts.js
@@ -4,20 +4,28 @@
         if (event.keyCode == 87 && !event.shiftKey) {
             chrome.runtime.sendMessage({method: "getOptions"}, function(response) {
                 var tab_count = response.fso_tabcount
-                var entriesPath = '//div[@class="EntryList__chunk"]//div[contains(@class, "TitleOnlyEntry")]';
-                var starPath = './/button[contains(@class, "EntryReadLaterButton--saved")]';
-                // 2022/12/7
+                var entriesPath = '//div[@class="EntryList__chunk"]/article/div[contains(@class, "TitleOnlyEntry")]';
+                var starPath = './/button';
+                var notOpenPath = './/*[name()="svg" and contains(@class, "color-accent")]';
                 var linkPath = './/div[@class="TitleOnlyEntry__content"]/a'
                 var entries = document.evaluate(entriesPath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
                 // console.log("entries=" + entries.snapshotLength);
                 for (var i = 0, m = tab_count; i < entries.snapshotLength && m > 0; i++) {
                     var entry = entries.snapshotItem(i);
                     var stars = document.evaluate(starPath, entry, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-                    //  console.log("stars=" + stars.snapshotLength);
+                    // console.log("stars=" + stars.snapshotLength);
                     if (stars.snapshotLength  == 0) {
                         // skip unmarked star entry
                         continue;
                     }
+                    var star = stars.snapshotItem(0);
+                    var notOpen = document.evaluate(notOpenPath, star, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+                    // console.log("notOpen=" + notOpen.snapshotLength);                    
+                    if (notOpen.snapshotLength == 0) {
+                        // already Open
+                        continue;
+                    }
+                    
                     m--
                     var links = document.evaluate(linkPath, entry, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
                     //  console.log("links=" + links.snapshotLength);
@@ -25,7 +33,6 @@
                         var link = links.snapshotItem(0);
                         window.open(link.href);  
                     }
-                    var star = stars.snapshotItem(0);
                     var event = document.createEvent('MouseEvents');
                     event.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
                     star.dispatchEvent(event);                    
